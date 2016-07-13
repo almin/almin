cf342ac9a92038233dd1c898a73f455380323a6d
azu <azuciao@gmail.com>
2016-07-13 20:39:04 +0900

    chore: remove commet

--- packages/almin-logger/src/performance-now.js
+++ packages/almin-logger/src/performance-now.js
@@ -1,9 +1,4 @@
 var performanceNow;
-/**
- * Detect if we can use `window.performance.now()` and gracefully fallback to
- * `Date.now()` if it doesn't exist. We need to support Firefox < 15 for now
- * because of Facebook's testing infrastructure.
- */
 if (typeof performance !== "undefined" && performance.now) {
     performanceNow = () => performance.now();
 } else {
