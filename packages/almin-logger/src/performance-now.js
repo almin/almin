65dc9990d090a6d66ca8bc43557fc135a6291305
azu <azuciao@gmail.com>
2016-07-13 20:36:00 +0900

    fix(polyfill): not use `bind` function to Date

--- packages/almin-logger/src/performance-now.js
+++ packages/almin-logger/src/performance-now.js
@@ -1,4 +1,12 @@
-const now = typeof performance !== "undefined"
-    ? performance.now.bind(performance)
-    : Date.now.bind(Date);
-export default now
+var performanceNow;
+/**
+ * Detect if we can use `window.performance.now()` and gracefully fallback to
+ * `Date.now()` if it doesn't exist. We need to support Firefox < 15 for now
+ * because of Facebook's testing infrastructure.
+ */
+if (typeof performance !== "undefined" && performance.now) {
+    performanceNow = () => performance.now();
+} else {
+    performanceNow = () => Date.now();
+}
+export default performanceNow
