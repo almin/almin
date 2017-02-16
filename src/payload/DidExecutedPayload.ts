1dfdcf6b412342b177957cc05514a140d77aa6f1
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-02-17 01:28:20 +0900

    Convert DidExecutedPayload to TypeScript

---
+++ src/payload/DidExecutedPayload.ts
@@ -0,0 +1,24 @@
+// LICENSE : MIT
+"use strict";
+import Payload from "./Payload";
+
+export const TYPE = "ALMIN__DID_EXECUTED_EACH_USECASE__";
+
+export default class DidExecutedPayload extends Payload {
+
+    static get Type(): typeof TYPE {
+        return TYPE;
+    }
+
+    /**
+     * the value is returned by the useCase
+     * Maybe Promise or some value or undefined.
+     * If you want to know unwrapped promise value, please see CompletedPayload.
+     */
+    value: any | undefined;
+
+    constructor({ value }: { value?: any; }) {
+        super({ type: DidExecutedPayload.Type });
+        this.value = value;
+    }
+}
