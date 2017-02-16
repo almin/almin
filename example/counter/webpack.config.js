a0398310df6340af30fd54dc99e843bbf6bbe5c3
azu <azuciao@gmail.com>
2017-02-17 01:11:53 +0900

    chore(example): Update example

--- example/counter/webpack.config.js
+++ example/counter/webpack.config.js
@@ -1,10 +1,7 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    entry: [
-        "./src/index.js"
-    ],
+    entry: "./src/index.js",
     devtool: process.env.WEBPACK_DEVTOOL || "source-map",
     output: {
         path: path.join(__dirname, "public", "build"),
@@ -12,19 +9,15 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        loaders: [
-            {test: /\.json$/, loader: "json"},
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel",
-                query: {
+                loader: "babel-loader",
+                options: {
                     cacheDirectory: true
                 }
             }
-        ],
-        // Allow expression as dependency to suppress warnings
-        // http://webpack.github.io/docs/configuration.html#automatically-created-contexts-defaults-module-xxxcontextxxx
-        exprContextCritical: false
+        ]
     }
 };
--- example/todomvc/webpack.config.js
+++ example/todomvc/webpack.config.js
@@ -1,29 +1,23 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    entry: [
-        "./src/index.js"
-    ],
-    devtool: process.env.WEBPACK_DEVTOOL || "eval",
+    entry: "./src/index.js",
+    devtool: process.env.WEBPACK_DEVTOOL || "source-map",
     output: {
         path: path.join(__dirname, "public", "build"),
         publicPath: "/build/",
         filename: "bundle.js"
     },
     module: {
-        loaders: [
-            {test: /\.json$/, loader: "json-loader"},
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel",
-                query: {
+                loader: "babel-loader",
+                options: {
                     cacheDirectory: true
                 }
             }
-        ],
-        // to avoid warning by power-assert-formatter
-        exprContextCritical: false
+        ]
     }
 };
