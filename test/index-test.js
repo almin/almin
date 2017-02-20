8fb02e13e626de27f0724d30e0910e8039eff2c6
azu <azu@users.noreply.github.com>
2017-02-20 17:46:34 +0900

    feat(typescript): add d.ts to pacakge.json (#78)
    * feat(typescript): add d.ts to pacakge.json
    
    * fix(package): add `--copy-files` to babel build
    
    * refactor(package):  add `build:src` and `build:lib`

---
+++ test/index-test.js
@@ -0,0 +1,17 @@
+// MIT © 2017 azu
+"use strict";
+const assert = require("assert");
+import {Context} from "../lib/index";
+describe("index", () => {
+    it("should export as named import", () => {
+        assert.ok(Context !== undefined);
+    });
+    it("could required + get property", () => {
+        const Store = require("../lib/index").Store;
+        assert.ok(Store !== undefined);
+    });
+    it("could required with destructuring", () => {
+        const {Store} = require("../lib/index");
+        assert.ok(Store !== undefined);
+    });
+});
