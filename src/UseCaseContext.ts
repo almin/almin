ec9eebea15944f970f0a717a11e4df56210ce56a
azu <azu@users.noreply.github.com>
2017-03-28 23:56:02 +0900

    fix(UseCaseContext): support functional UseCase (#135)

--- src/UseCaseContext.ts
+++ src/UseCaseContext.ts
@@ -3,6 +3,8 @@
 
 import { UseCase } from "./UseCase";
 import { UseCaseExecutor } from "./UseCaseExecutor";
+import { FunctionalUseCase } from "./FunctionalUseCase";
+import { FunctionalUseCaseContext } from "./FunctionalUseCaseContext";
 const assert = require("assert");
 /**
  * Maybe, `UseCaseContext` is invisible from Public API.
@@ -65,14 +67,34 @@ export class UseCaseContext {
      * }
      * ```
      */
-    useCase(useCase: UseCase): UseCaseExecutor {
+    useCase(useCase: (context: FunctionalUseCaseContext) => Function): UseCaseExecutor;
+    useCase(useCase: UseCase): UseCaseExecutor;
+    useCase(useCase: any): UseCaseExecutor {
         if (process.env.NODE_ENV !== "production") {
             assert(useCase !== this._dispatcher, `the useCase(${useCase}) should not equal this useCase(${this._dispatcher})`);
         }
-        return new UseCaseExecutor({
-            useCase,
-            parent: UseCase.isUseCase(this._dispatcher) ? this._dispatcher : null,
-            dispatcher: this._dispatcher
-        });
+        if (UseCase.isUseCase(useCase)) {
+            return new UseCaseExecutor({
+                useCase,
+                parent: UseCase.isUseCase(this._dispatcher) ? this._dispatcher : null,
+                dispatcher: this._dispatcher
+            });
+        } else if (typeof useCase === "function") {
+            if (process.env.NODE_ENV !== "production") {
+                // When pass UseCase constructor itself, throw assertion error
+                assert.ok(Object.getPrototypeOf && Object.getPrototypeOf(useCase) !== UseCase,
+                    `Context#useCase argument should be instance of UseCase.
+The argument is UseCase constructor itself: ${useCase}`
+                );
+            }
+            // function to be FunctionalUseCase
+            const functionalUseCase = new FunctionalUseCase(useCase, this._dispatcher);
+            return new UseCaseExecutor({
+                useCase: functionalUseCase,
+                parent: UseCase.isUseCase(this._dispatcher) ? this._dispatcher : null,
+                dispatcher: this._dispatcher
+            });
+        }
+        throw new Error(`UseCaseContext#useCase argument should be UseCase: ${useCase}`);
     }
 }
---
+++ test/UseCaseContext-test.js
@@ -0,0 +1,32 @@
+// MIT © 2017 azu
+"use strict";
+const assert = require("assert");
+import { UseCase, Dispatcher, Context, Store } from "../lib/";
+import { functionalUseCase } from "./use-case/FunctionalUseCase";
+describe("UseCaseContext", () => {
+    context("useCase", () => {
+        it("should execute functional useCase", () => {
+            const expectedPayload = {
+                "type": "expected"
+            };
+            const dispatched = [];
+            class ParentUseCase extends UseCase {
+                execute() {
+                    return this.context.useCase(functionalUseCase).execute(expectedPayload.type);
+                }
+            }
+            const dispatcher = new Dispatcher();
+            const context = new Context({
+                dispatcher,
+                store: new Store()
+            });
+            context.onDispatch((payload) => {
+                dispatched.push(payload);
+            });
+            return context.useCase(new ParentUseCase()).execute().then(() => {
+                assert(dispatched.length === 1);
+                assert.deepEqual(dispatched[0], expectedPayload);
+            });
+        });
+    });
+});
