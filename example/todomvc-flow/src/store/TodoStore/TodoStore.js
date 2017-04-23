37e6203282c587fa2d1569ba56562956d0f5f3ba
azu <azuciao@gmail.com>
2017-04-23 16:01:19 +0900

    refactor(example): migrate todomvc-flow to almin 0.12

--- example/todomvc-flow/src/store/TodoStore/TodoStore.js
+++ example/todomvc-flow/src/store/TodoStore/TodoStore.js
@@ -1,55 +1,31 @@
 // @flow
 "use strict";
-import {Store} from "almin";
-import type {DispatcherPayload} from "almin";
+import { Store } from "almin";
+import type { DispatcherPayload } from "almin";
 import TodoState from "./TodoState";
 import TodoList from "../../domain/TodoList/TodoList";
 import typeof TodoListRepository from "../../infra/TodoListRepository";
 export default class TodoStore extends Store {
     state: TodoState;
 
-    constructor({todoListRepository}: {todoListRepository: TodoListRepository}) {
+    constructor({ todoListRepository }: { todoListRepository: TodoListRepository }) {
         super();
         this.state = new TodoState();
-        // when todoRepository is changed, try to update state
-        todoListRepository.onChange(this._onChange.bind(this));
-        // when UseCase dispatch event, try to update state
-        this.onDispatch(this._onDispatch.bind(this));
+        this.todoListRepository = todoListRepository;
     }
 
-    getState(): {todoState: TodoState} {
-        return {
-            todoState: this.state
-        };
-    }
-
-    /**
-     * set newState if the state is changed
-     * @param {TodoState} newState
-     * @private
-     */
-    _setState(newState: TodoState): void {
-        if (newState !== this.state) {
+    receivePayload(payload: DispatcherPayload) {
+        const todoList: TodoList = this.todoListRepository.lastUsed();
+        if (!todoList) {
+            return;
+        }
+        const newState = this.state.merge(todoList).reduce(payload);
+        if (this.shouldStateUpdate(this.state, newState)) {
             this.state = newState;
-            this.emitChange();
         }
     }
 
-    /**
-     * @param {TodoList} todoList
-     * @private
-     */
-    _onChange(todoList: TodoList): void {
-        const newState = this.state.merge(todoList);
-        this._setState(newState);
-    }
-
-    /**
-     * @param {DispatcherPayload} payload
-     * @private
-     */
-    _onDispatch(payload: DispatcherPayload): void {
-        const newState = this.state.reduce(payload);
-        this._setState(newState);
+    getState(): { todoState: TodoState } {
+        return this.state;
     }
 }
