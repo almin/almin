b18aef52bb4a0fef5fabf4a7d85b42449665fe97
azu <azuciao@gmail.com>
2017-04-30 10:28:16 +0900

    feat(TypeScript): add `StoreToState` utility type converter

--- src/UILayer/StoreGroupTypes.ts
+++ src/UILayer/StoreGroupTypes.ts
@@ -12,3 +12,28 @@ export type StateMap<T> = {
     // Now, T[P] is State
     [P in keyof T]: T[P]
 };
+
+/**
+ * Utility type function that create state mapping from store mapping.
+ *
+ * DO NOT USE the returned value.
+ * It should be used for typing.
+ *
+ * ## Example
+ *
+ * ```ts
+ * import { StoreGroupTypes } from "almin";
+ * // store mapping
+ * const storeMapping = {
+ *    appState: new AppStore({ appRepository }),
+ *    counterState: new CounterStore({ appRepository })
+ * };
+ * // state mapping
+ * const stateMapping = StoreGroupTypes.StoreToState(storeMapping);
+ * // typeof StoreGroup state
+ * export type AppStoreGroupState = typeof stateMapping;
+ * ```
+ */
+function StoreToState<T>(mapping: StoreMap<T>): StateMap<T> {
+    return mapping as any;
+}
