e10733b37a7218b41438af3a6f73d525a0cb0357
azu <azuciao@gmail.com>
2017-05-03 15:45:45 +0900

    test(example): Add CounterState test

--- examples/counter/test/CounterStore-test.js
+++ examples/counter/test/CounterStore-test.js
@@ -3,7 +3,7 @@
 const assert = require("assert");
 import IncrementalCounterUseCase from "../src/usecase/IncrementalCounterUseCase";
 import { CounterStore } from "../src/store/CounterStore";
-import { Dispatcher, Context } from "almin";
+import { Dispatcher, Context, StoreGroup } from "almin";
 describe("CounterStore", function() {
     context("when IncrementalCounterUseCase is executed", function() {
         it("should new state was count up", function() {
@@ -11,7 +11,9 @@ describe("CounterStore", function() {
             const store = new CounterStore();
             const context = new Context({
                 dispatcher: new Dispatcher(),
-                store
+                store: new StoreGroup({
+                    counter: store
+                })
             });
             // then
             return context.useCase(useCase).execute().then(() => {
