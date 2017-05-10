630fe17a9bdeec01770b889199d215bd9073b182
azu <azu@users.noreply.github.com>
2017-05-10 12:32:01 +0900

    fix(almin): fix warning message format (#195)

--- packages/almin/test/FunctionalUseCase-test.js
+++ packages/almin/test/FunctionalUseCase-test.js
@@ -1,37 +1,48 @@
 // LICENSE : MIT
 "use strict";
-const assert = require("power-assert");
+const assert = require("assert");
 const sinon = require("sinon");
 import { UseCase } from "../lib/UseCase";
 import { createEchoStore } from "./helper/EchoStore";
 import { Dispatcher } from "../lib/Dispatcher";
-import { Store } from "../lib/Store";
 import { Context } from "../lib/Context";
-import { UseCaseContext } from "../lib/UseCaseContext";
 
 describe("FunctionalUseCase", function() {
-    context("warning", () => {
-        class WriteToTextlintrcUseCase extends UseCase {
-            static create() {
-                return new this();
-            }
+    let consoleErrorStub = null;
+    beforeEach(() => {
+        consoleErrorStub = sinon.stub(console, "error");
+    });
+    afterEach(() => {
+        consoleErrorStub.restore();
+    });
+    context("When passing wrong functional UseCase", () => {
+        it("should show warning and throw Error", () => {
+            class WriteToTextlintrcUseCase extends UseCase {
+                static create() {
+                    return new this();
+                }
 
-            constructor() {
-                super();
-            }
+                constructor() {
+                    super();
+                }
 
-            execute() {
+                execute() {
+                }
             }
-        }
-        const dispatcher = new Dispatcher();
-        const context = new Context({
-            dispatcher,
-            store: createEchoStore({ echo: { "a": "a" } })
+            const dispatcher = new Dispatcher();
+            const context = new Context({
+                dispatcher,
+                store: createEchoStore({ echo: { "a": "a" } })
+            });
+            assert.throws(() => {
+                context.useCase(WriteToTextlintrcUseCase.create).execute();
+                //                                            ~~
+                //                                         missing call ()
+            }, Error);
+            // assert warning message
+            assert(consoleErrorStub.called);
+            const warningMessage = consoleErrorStub.getCalls()[0].args[0];
+            assert(warningMessage.indexOf("Warning(UseCase): This is wrong Functional UseCase.") !== -1, warningMessage);
         });
-        assert.throws(() => {
-            context.useCase(WriteToTextlintrcUseCase.create).execute();
-            //                                            ~~
-            //                                         missing call ()
-        }, Error);
     });
 });
