a0f3302692d60a085a0548439375ce28cf0ef7e5
azu <azu@users.noreply.github.com>
2017-05-10 17:17:07 +0900

    fix(perf): fix benchmark test (#196)

--- perf/benchmark/test/benchmark-test.js
+++ perf/benchmark/test/benchmark-test.js
@@ -5,10 +5,18 @@ const assert = require("assert");
 const bench = require("../src/index");
 
 const AlminVersions = {
-    "0.9": require("almin-0.9"),
-    "current": require("almin")
+    "0.9": require("../almin-0.9"),
+    "current": require("../almin-current")
 };
 describe("benchmark", function() {
+    const orginalEnv = process.env.NODE_ENV;
+    beforeEach(() => {
+        process.env.NODE_ENV = "production"
+    });
+    afterEach(() => {
+        process.env.NODE_ENV = orginalEnv
+    });
+
     it("Current version should be fastest", function(done) {
         this.timeout(50 * 1000);
         bench(AlminVersions, (benchmark) => {
