95491811656e49af134fced3f6738ae63aef4c75
azu <azu@users.noreply.github.com>
2017-05-21 22:45:23 +0900

    refactor(example): use almin-react-container in shopping-cart (#199)

--- examples/shopping-cart/src/components/App.js
+++ examples/shopping-cart/src/components/App.js
@@ -1,40 +1,17 @@
 import React from "react";
 import CartContainer from "./CartContainer";
 import ProductsContainer from "./ProductsContainer";
-import AppLocator from "../AppLocator";
-// UseCase
-import InitializeCustomerUseCase from "../usecase/Initial/InitializeCustomerUseCase";
-import InitializeProductUseCase from "../usecase/Initial/InitializeProductUseCase";
+
 class App extends React.Component {
-    constructor(...args) {
-        super(...args);
-        this.state = AppLocator.context.getState();
-    }
-
-    componentDidMount() {
-        const context = AppLocator.context;
-        // when change store, update component
-        const onChangeHandler = (stores) => {
-            this.setState(context.getState());
-        };
-        context.onChange(onChangeHandler);
-
-        context.useCase(InitializeCustomerUseCase.create()).execute().then(() => {
-            return context.useCase(InitializeProductUseCase.create()).execute();
-        });
-    }
-
-
     render() {
-        const state = AppLocator.context.getState();
         /**
          * @type {CartState}
          */
-        const CartState = state.CartState;
+        const CartState = this.props.CartState;
         /**
          * @type {ProductState}
          */
-        const ProductState = state.ProductState;
+        const ProductState = this.props.ProductState;
         return (
             <div>
                 <ProductsContainer products={ProductState.products}/>
--- examples/shopping-cart/src/index.js
+++ examples/shopping-cart/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AlminReactContainer from "almin-react-container";
+import AlminLogger from "almin-logger";
 import App from "./components/App";
 import AppLocator from "./AppLocator";
-import AlminLogger from "almin-logger";
 // store
 import AppStore from "./stores/AppStore";
+// UseCase
+import InitializeCustomerUseCase from "./usecase/Initial/InitializeCustomerUseCase";
+import InitializeProductUseCase from "./usecase/Initial/InitializeProductUseCase";
 // context
-import {Context}  from "almin";
-import {Dispatcher} from "almin";
+import { Context, Dispatcher } from "almin";
 // instances
 const dispatcher = new Dispatcher();
 // context connect dispatch with stores
@@ -15,16 +18,24 @@ const appContext = new Context({
     dispatcher,
     store: AppStore.create()
 });
-// start logger
-const logger = new AlminLogger();
-logger.startLogging(appContext);
-// update global context
+if (process.env.NODE_ENV !== "production") {
+    // start logger
+    const logger = new AlminLogger();
+    logger.startLogging(appContext);
+    AppLocator.alminLogger = logger;
+}
+// set global context
 AppLocator.context = appContext;
-AppLocator.alminLogger = logger;
-ReactDOM.render(
-    React.createElement(App, {
-        appContext
-    }),
-    document.getElementById("flux-app")
-);
-
+// Initialize application domain
+AppLocator.context.useCase(InitializeCustomerUseCase.create()).execute()
+.then(() => {
+    return AppLocator.context.useCase(InitializeProductUseCase.create()).execute();
+}).then(() => {
+    // StoreGroup#getState to <App .{..state} />
+    const Bootstrap = AlminReactContainer.create(App, AppLocator.context);
+    // Initial render
+    ReactDOM.render(
+        <Bootstrap/>,
+        document.getElementById("flux-app")
+    );
+});
