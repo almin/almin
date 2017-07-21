20fdf8618db1e36f8155a86352fc736142275b6a
azu <azuciao@gmail.com>
2017-07-21 17:40:35 +0900

    refactor(almin) rename id generator

--- packages/almin/src/UnitOfWork/UnitOfWorkIdGenerator.ts
+++ packages/almin/src/UnitOfWork/UnitOfWorkIdGenerator.ts
@@ -1,11 +1,11 @@
 /**
  * UseCase incremental count is for Unique ID.
  */
-let _UseCaseCount: number = 0;
+let _UnitOfWorkId: number = 0;
 /**
  * create new id
  */
 export const generateNewId = (): string => {
-    _UseCaseCount++;
-    return String(`UnitOfWork__${_UseCaseCount}`);
+    _UnitOfWorkId++;
+    return String(`UnitOfWork__${_UnitOfWorkId}`);
 };
