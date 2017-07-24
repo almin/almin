ae8cddb2bad54449ace0e02e2a9bcd088cb5de85
azu <azuciao@gmail.com>
2017-07-24 09:04:57 +0900

    chore(example): update meta data

--- examples/todomvc-typescript/webpack.config.js
+++ examples/todomvc-typescript/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
-
+const { CheckerPlugin } = require("awesome-typescript-loader");
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.tsx",
     devtool: process.env.WEBPACK_DEVTOOL || "source-map",
     output: {
         path: path.join(__dirname, "public", "build"),
         publicPath: "/build/",
         filename: "bundle.js"
+    },
+    // Currently we need to add '.ts' to the resolve.extensions array.
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
     module: {
         rules: [
@@ -15,5 +19,6 @@ module.exports = {
                 loader: "awesome-typescript-loader"
             }
         ]
-    }
+    },
+    plugins: [new CheckerPlugin()]
 };
