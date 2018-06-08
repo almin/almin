66a773d0fcd183ec6d3642dea9caaf46b8303265
azu <azuciao@gmail.com>
2018-06-08 22:03:58 +0900

    fix(almin-react-container): support TypeScript 2.9

--- packages/almin-react-container/src/almin-react-container.tsx
+++ packages/almin-react-container/src/almin-react-container.tsx
@@ -5,15 +5,10 @@ import { Context } from "almin";
 import { shallowEqual } from "shallow-equal-object";
 
 // Diff typing utilities
-// https://qiita.com/cotttpan/items/999fe07d079298c35e0c
-export type AlminReactContainerDiffKey<T extends string, U extends string> = ({ [P in T]: P } &
-    { [P in U]: never } & { [x: string]: never })[T];
-
-export type AlminReactContainerOmit<T, K extends keyof T> = Pick<T, AlminReactContainerDiffKey<keyof T, K>>;
-
-export type AlminReactContainerDiff<T, U> = AlminReactContainerOmit<T, keyof U & keyof T>;
-// T - U
-export type AlminReactContainerWeakDiff<T, U> = AlminReactContainerDiff<T, U> & { [K in keyof U & keyof T]?: T[K] };
+// Remove types from T that are assignable to U
+export type AlminReactContainerDiff<T, U> = T extends U ? never : T;
+// https://stackoverflow.com/questions/49564342/typescript-2-8-remove-properties-in-one-type-from-another
+export type AlminReactContainerObjectDiff<T, U> = Pick<T, AlminReactContainerDiff<keyof T, keyof U>>;
 
 export class AlminReactContainer {
     // T is Custom props
@@ -29,7 +24,7 @@ export class AlminReactContainer {
         const componentName = WrappedComponent.displayName || WrappedComponent.name;
         // AlminContainer's props is <T - P> type
         // T is State of Store, P is custom props of the `WrappedComponent`
-        return class AlminContainer extends React.Component<AlminReactContainerWeakDiff<T, P>> {
+        return class AlminContainer extends React.Component<AlminReactContainerObjectDiff<T, P>> {
             static displayName = `AlminContainer(${componentName})`;
 
             state: P;
